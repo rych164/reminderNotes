5204de855f37793d1309b01b79dc49dd798285f9
rych164 <wawrzyniec.waliczek@hewalex.pl>
2025-01-06 16:36:28 +0100

    poprawne uruchamianie się aplikacji na telefonie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -50,8 +51,11 @@ android {
 }
 
 dependencies {
+    // Existing...
     implementation(libs.material.icons.core)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -60,7 +64,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.room.common)
-    implementation(libs.androidx.room.ktx)
+
+    // --------------------------------------------------------------
+    // THIS is what you are missing for KSP to generate Room classes:
+    ksp(libs.androidx.room.compiler)
+    // --------------------------------------------------------------
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
